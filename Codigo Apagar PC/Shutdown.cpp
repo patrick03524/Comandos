174d3b49311e3b6a80463a6a9e44123556a9cf9e
Patrick Márquez <dibujanteanime024@gmail.com>
2019-04-22 11:30:11 -0500

    N-NANDATO KORE WA MASAKA
    xd

---
+++ Codigo Apagar PC/Shutdown.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include<stdio.h>
+#include <stdlib.h>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	system("C:\\WINDOWS\\System32\\shutdown /sxddd");
+	return 0;
+}
+
